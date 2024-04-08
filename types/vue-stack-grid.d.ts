8a0d9d04bf251c0cad29ab5579bd0b8339134e02
chiribuc <me@crobert.ro>
2024-04-08 06:51:13 +0300

    add types
    Signed-off-by: chiribuc <me@crobert.ro>

---
+++ types/vue-stack-grid.d.ts
@@ -0,0 +1,6 @@
+// vue-stack-grid.d.ts
+declare module '@crob/vue-stack-grid' {
+    import { DefineComponent } from 'vue';
+    const StackGrid: DefineComponent<{}, {}, any>;
+    export default StackGrid;
+}
