4671e6b9d6b44dd8aa1d409d3f8b46a6b45119aa
chiribuc <me@crobert.ro>
2024-12-09 13:01:29 +0200

    chore: remove unnecessary configuration files
    - deleted babel.config.json as it's no longer needed
    - removed jest.config.cjs due to changes in testing strategy
    - eliminated jest.setup.cjs.js following updates in test setup
    - got rid of package-lock.json after dependency cleanup

--- types/vue-stack-grid.d.ts
+++
@@ -1,6 +0,0 @@
-// vue-stack-grid.d.ts
-declare module '@crob/vue-stack-grid' {
-    import { DefineComponent } from 'vue';
-    const StackGrid: DefineComponent<{}, {}, any>;
-    export default StackGrid;
-}
